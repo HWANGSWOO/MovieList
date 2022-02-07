7f95c2166f751ccde0a95e525b163651bba6798c
1401150@office365.kunsan.ac.kr <1401150@office365.kunsan.ac.kr>
2022-02-07 16:58:01 +0900

    초기 커밋.

---
+++ MovieList/Models/MovieGenreViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MovieList.Models
+{
+    public class MovieGenreViewModel
+    {
+        /// <summary>
+        /// 영화 목록
+        /// </summary>
+        public List<Movie>? Movies { get; set; }
+        /// <summary>
+        /// 장르 목록
+        /// </summary>
+        public SelectList? Genres { get; set; }
+        /// <summary>
+        /// 선택한 장르가 담김
+        /// </summary>
+        public string? MovieGenre { get; set; }
+        /// <summary>
+        /// 검색 텍스트 상자에 입력한 텍스트가 담긴 SearchString
+        /// </summary>
+        public string? SearchString { get; set; }
+    }
+}
